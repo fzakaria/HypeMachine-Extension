5855e476871c4a37a0b3a455fff4b4692bc827b1
Fernando Gonzalez <Fernando@fernando-gonzalezs-macbook-pro.local>
2011-04-19 23:19:08 -0700

    Updated changelog.

--- extension.js
+++ extension.js
@@ -1,32 +1,31 @@
 // Extension main script
 
-var downloadImageUrl = chrome.extension.getURL("/images/downloadButton.png");
 var stylesheetUrl = chrome.extension.getURL("hypestyles.css");
 
 // This is all the JS that will be injected in the document body
-var main = function(downloadImageUrl) {
+var main = function() {
 	// Adds a download button next to each track
 	var buttonScript = function() {
 		// Wait for the tracks script to load
 		var tracks = trackList[document.location.href];
 		if (tracks === undefined || tracks.length < 1) {
-			setTimeout(buttonScript, 1000);
+			setTimeout(buttonScript, 1);
 		} else {
 			// Check if this particular page has been processed
 			// through a previous call
-			if ($$('.cext').length < 1) {
+			if ($$('.dl').length < 1) {
 				$$('ul.tools').each(function(track, index) {
 					var songUrl = "/serve/play/"+tracks[index].id+"/";
-					songUrl += tracks[index].key + ".mp3";
-					track.insertAdjacentHTML("AfterBegin", '<li class="cext"><a href="'+songUrl+'"> <img class="cext" src="'+downloadImageUrl+'"></a></li>');
+					songUrl += tracks[index].key;
+					track.insertAdjacentHTML("AfterBegin", '<li class="dl"><table class="spacer"></table><a href="'+songUrl+'"><table class="arrow"><tr></td><td><div class="rect"></div></td></td></tr><tr><td class="tri"></td></tr></table></a></li>');
 					});
 			}		
 		}
 	};
-		
+
 	// Run it right away
 	buttonScript();
-	
+
 	// Re-display buttons after an Ajax update is complete
 	Ajax.Responders.register({
 		onComplete: buttonScript
@@ -36,7 +35,7 @@ var main = function(downloadImageUrl) {
 // Lets create the script objects
 var injectedScript = document.createElement('script');
 injectedScript.type = 'text/javascript';
-injectedScript.text = '('+main+')("'+downloadImageUrl+'");';
+injectedScript.text = '('+main+')("");';
 (document.body || document.head).appendChild(injectedScript);
 
 //Lets create the CSS object. This has to be done this way rather than the manifest.json
